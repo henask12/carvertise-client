51de6939c3be89eeff60d9ec5bfb9375c200b638
Henok Mekonnen <henokmekonnen5@gmail.com>
2023-09-24 12:04:09 +0300

    updated header colors

--- carvertise-client/src/components/Layout/Header.js
+++ carvertise-client/src/components/Layout/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
   return (
-    <header className="bg-blue-500 py-4 px-6 flex justify-between items-center">
+    <header className="bg-cyan-700 py-4 px-6 flex justify-between items-center">
       {/* Left Side: Carvertise Logo */}
       <div className="text-white text-lg font-semibold">
         <span className="text-yellow-500">Carvertise</span> Logo
@@ -13,6 +13,7 @@ function Header() {
         <a href="#" className="text-white hover:text-yellow-500 transition duration-300">
           Brands & Agencies
         </a>
+        <span className="text-white hover:text-yellow-500 transition duration-300"> |</span>
         <a href="#" className="text-white hover:text-yellow-500 transition duration-300">
           Drivers
         </a>
