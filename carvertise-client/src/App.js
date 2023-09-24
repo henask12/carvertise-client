2c9d966b55281d487360121ff7104a2bc12012d5
Henok Mekonnen <henokmekonnen5@gmail.com>
2023-09-24 12:04:28 +0300

    updated app file to include header and footer components

--- carvertise-client/src/App.js
+++ carvertise-client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Footer from './components/Layout/Footer';
 import Header from './components/Layout/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
