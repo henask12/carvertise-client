80907374209e6d5186bda4fa6f0b3b1bedda0fdc
Henok Mekonnen <henokmekonnen5@gmail.com>
2023-09-25 15:35:05 +0300

    added just dummy data

---
+++ carvertise-client/src/components/Layout/choouseUsData.js
@@ -0,0 +1,20 @@
+const choosedummyData = [
+  {
+    id: 1,
+    title: "Media Measurement",
+    description:
+      "We offer advanced media measurement solutions to track your brand's performance.",
+    image: "https://i.ytimg.com/vi/uKOiHwht1FA/maxresdefault.jpg",
+  },
+  {
+    id: 2,
+    title: "Expert Team",
+    description:
+      "Our team consists of experts in the field with years of experience.",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTorBKo4zemh7Dai5VqdqYTGCjujVbAa7lP8Q&usqp=CAU",
+  },
+  // Add more rows as needed
+];
+
+export default choosedummyData;
