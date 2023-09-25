a1e166f3c841e685d6a41a52bd8f48a26002241b
Henok Mekonnen <henokmekonnen5@gmail.com>
2023-09-25 15:06:32 +0300

    just dummy data for about us page

---
+++ carvertise-client/src/components/Layout/dummyData.js
@@ -0,0 +1,25 @@
+const dummyData = [
+  {
+    id: 1,
+    name: "John Doe",
+    role: "Founder",
+    description: "this is just a place holder for the person to be created",
+    image: "https://via.placeholder.com/150",
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    role: "Designer",
+    description: "this is just a place holder for the person to be created",
+    image: "https://via.placeholder.com/150",
+  },
+  {
+    id: 3,
+    name: "Bob Johnson",
+    role: "Developer",
+    description: "this is just a place holder for the person to be created",
+    image: "https://via.placeholder.com/150",
+  },
+];
+
+export default dummyData;
