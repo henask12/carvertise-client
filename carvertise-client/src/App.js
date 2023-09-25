8d4c8c9702d2573e06cb461d3709a2d2bd87b8b6
Henok Mekonnen <henokmekonnen5@gmail.com>
2023-09-25 12:17:56 +0300

    update app js file

--- carvertise-client/src/App.js
+++ carvertise-client/src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
-import Footer from './components/Layout/Footer';
-import Header from './components/Layout/Header';
+import "./App.css";
+import Footer from "./components/Layout/Footer";
+import Header from "./components/Layout/Header";
+import HeroSection from "./components/Layout/Hero";
+import Testimonials from "./components/Layout/Testimonials";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <HeroSection />
+      <Testimonials />
       <Footer />
     </div>
   );
