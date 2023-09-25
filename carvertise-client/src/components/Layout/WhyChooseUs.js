2a7c8bbba1d296dadc42367553f1d2df0857e3d5
Henok Mekonnen <henokmekonnen5@gmail.com>
2023-09-25 16:03:32 +0300

    update on choose us page

--- carvertise-client/src/components/Layout/WhyChooseUs.js
+++ carvertise-client/src/components/Layout/WhyChooseUs.js
@@ -6,7 +6,7 @@ function WhyChooseUs() {
   return (
     <div className="bg-gray-100 min-h-screen p-4">
       <h1 className="text-4xl font-bold text-center">Why Choose Us</h1>
-      <div className=" p-24 pt-4 flex justify-center items-center">
+      <div className="md:p-24 pt-4 flex justify-center items-center">
         <div className="container mx-auto">
           {choosedummyData.map((item) => (
             <div key={item.id} className="m-4">
